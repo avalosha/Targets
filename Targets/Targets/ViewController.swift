f18b03f4eb4bf842ba3bff4f87252a68052446c6
Esfera Estudios <github@sferea.com>
2023-09-27 12:40:43 -0600

    Add target for Development.

--- Targets/Targets/ViewController.swift
+++ Targets/Targets/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+#if DEV
+        print("This is a Development Environment")
+#else
+        print("This is a Production Environment")
+#endif
     }
 
 
