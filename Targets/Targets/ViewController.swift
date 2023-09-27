66995daba8800f29f854f7f321236cbf9c2c7c27
Esfera Estudios <github@sferea.com>
2023-09-27 17:56:48 -0600

    Reinstall Firebase.

--- Targets/Targets/ViewController.swift
+++ Targets/Targets/ViewController.swift
@@ -6,19 +6,29 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 #if DEV
-        print("This is a Development Environment")
+        print("*** This is a Development Environment ***")
+        Analytics.logEvent("Development", parameters: nil)
+        testBtn.setTitle("Development", for: .normal)
 #else
-        print("This is a Production Environment")
+        print("*** This is a Production Environment ***")
+        Analytics.logEvent("Production", parameters: nil)
+        testBtn.setTitle("Production", for: .normal)
 #endif
     }
 
-
+    @IBAction func onClickTestBtn(_ sender: Any) {
+        print("Click btn")
+        Analytics.logEvent("click_btn", parameters: nil)
+    }
 }
 
